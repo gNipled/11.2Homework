8c5ac1e20ff075451d6db195ff72805e35e97fd1
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-08-29 22:21:06 +0900

    Added new function to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def string_upper(string):
     return str(string).upper()
+
+
+def string_first_letter_upper(string):
+    """Making first letter of input string upper case"""
+    return str(string).title()
